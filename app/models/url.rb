abebc0454856055d1b2da1da3503e93bf9607a25
Apprentice <apprentice@devbootcamp.com>
2013-05-13 18:29:33 -0700

    lotsta stuff

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,17 @@
 class Url < ActiveRecord::Base
+  before_validation  :create_short_url
+  validates          :full, url: true
+  validates          :short, uniqueness: true
+  belongs_to         :user
 
-end
+  private
+  def create_short_url
+    return self if short
+    self.short = SecureRandom.hex(3)
+      # check does short url already exist?
+      if Url.where(short: short).empty?
+      else
+        create_short_url
+      end
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-  
+  validates :email, :confirmation => true
+  validates :email, :uniqueness => true
+  validates :password_hash, :presence => true
+  has_many  :urls
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
